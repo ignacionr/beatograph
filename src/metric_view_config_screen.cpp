c2ece53d77c92c84fa50ba15e3a98a3a65aaded5
Ignacio Rodriguez <ignacionr@gmail.com>
2024-10-18 10:09:01 +0300

    Refactor code to include metric_view_config_screen.cpp for metric view configuration

---
+++ src/metric_view_config_screen.cpp
@@ -0,0 +1,12 @@
+#include "metric_view_config_screen.hpp"
+
+void metric_view_config_screen::render()
+{
+    ImGui::InputFloat("Min value", &config.min_value);
+    ImGui::InputFloat("Max value", &config.max_value);
+    ImGui::InputFloat("Concern threshold", &config.concern_threshold);
+    ImGui::InputFloat("Warning threshold", &config.warning_threshold);
+    ImGui::ColorEdit4("Normal color", config.normal_color);
+    ImGui::ColorEdit4("Warning color", config.warning_color);
+    ImGui::ColorEdit4("Concern color", config.concern_color);
+}
--- src/metric_view_config_screen.hpp
+++ src/metric_view_config_screen.hpp
@@ -6,15 +6,7 @@
 
 struct metric_view_config_screen {
     metric_view_config_screen(metric_view_config &config) : config(config) {}
-    void render() {
-        ImGui::InputFloat("Min value", &config.min_value);
-        ImGui::InputFloat("Max value", &config.max_value);
-        ImGui::InputFloat("Concern threshold", &config.concern_threshold);
-        ImGui::InputFloat("Warning threshold", &config.warning_threshold);
-        ImGui::ColorEdit4("Normal color", config.normal_color);
-        ImGui::ColorEdit4("Warning color", config.warning_color);
-        ImGui::ColorEdit4("Concern color", config.concern_color);
-    }
+    void render();
 private:
     metric_view_config &config;
 };
