68b2e792e724a1759cd21a648f52f44420372070
Ignacio Rodriguez <ignacionr@gmail.com>
2024-10-23 15:43:25 +0300

    Refactor host_screen.hpp to display memory usage in performance metrics

--- src/host/screen.hpp
+++ src/host/screen.hpp
@@ -25,9 +25,27 @@ struct host_screen
         if (ImGui::CollapsingHeader("Performance Metrics")) {
             std::shared_ptr<metrics_model> model = host.metrics();
             if (model) {
-                for (auto const &[metric, values] : model->metrics) {
-                    metric_view_.render(metric, model->views[&metric], values);
+                auto available_bytes_pos = model->metrics.find({"node_memory_MemAvailable_bytes"});
+                if (available_bytes_pos != model->metrics.end()) {
+                    auto &available_bytes = available_bytes_pos->second;
+                    auto total_bytes_pos = model->metrics.find({"node_memory_MemTotal_bytes"});
+                    if (total_bytes_pos != model->metrics.end()) {
+                        auto &total_bytes = total_bytes_pos->second;
+                        auto available = std::accumulate(available_bytes.begin(), available_bytes.end(), 0.0, [](auto acc, auto const &mv) { return acc + mv.value; });
+                        auto total = std::accumulate(total_bytes.begin(), total_bytes.end(), 0.0, [](auto acc, auto const &mv) { return acc + mv.value; });
+                        if (total > 0.1) {
+                            ImGui::ProgressBar(static_cast<float>(available / total), ImVec2(0.0f, 0.0f));
+                            ImGui::SameLine(0.0f, ImGui::GetStyle().ItemInnerSpacing.x);
+                            ImGui::Text("Memory Usage: %.2f%%", 100.0 * available / total);
+                        }
+                    }
                 }
+                else {
+                    ImGui::Text("Memory Usage: N/A");
+                }
+            }
+            else {
+                ImGui::Text("Metrics not available");
             }
         }
         ImGui::Unindent();
