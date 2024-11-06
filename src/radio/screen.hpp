e03584f2c6238a473d27cc67456474ba947712cf
Ignacio Rodriguez <ignacionr@gmail.com>
2024-11-06 13:06:40 +0300

    Adjust dial height and improve layout for radio presets selection

--- src/radio/screen.hpp
+++ src/radio/screen.hpp
@@ -9,7 +9,7 @@
 
 namespace radio {
     struct screen {
-        static constexpr auto dial_height{300};
+        static constexpr auto dial_height{200};
 
         screen(host &h) : host_(h) {
             presets = host_.presets();
@@ -27,32 +27,45 @@ namespace radio {
 
             constexpr auto green = IM_COL32(0, 160, 0, 255);
             // constexpr auto white = IM_COL32(255, 255, 255, 255);
-            constexpr auto red = IM_COL32(255, 0, 0, 180);
+            constexpr auto red = IM_COL32(255, 0, 0, 255);
 
             auto const y_center = dial_height / 2;
-            auto const selectable_height = (dial_height - 50) / 4;
+            auto const selectable_height = dial_height / 7;
+            std::array<float, 4> row_y {
+                initial_pos.y + y_center - 2 * selectable_height - 5.0f, 
+                initial_pos.y + y_center - selectable_height - 5.0f, 
+                initial_pos.y + y_center + selectable_height, 
+                initial_pos.y + y_center + 2 * selectable_height
+            };
 
             // std::array<int, 4> row_x{};
             int current_row{1};
+            auto const x_unit = dial_width / (presets.size() + 2);
             for (auto ip{0}; ip < presets.size(); ++ip) {
-                auto const x = initial_pos.x + dial_width * ip / presets.size();
+                auto const x = initial_pos.x + ip * x_unit;
                 auto start_x = x + 2;
-                if (current_row <= 1) {
-                    draw_list->AddLine({start_x + 3, initial_pos.y + y_center - 1}, {start_x + 3, initial_pos.y + y_center - 4}, green);
-                }
-                else {
-                    draw_list->AddLine({start_x + 3, initial_pos.y + y_center + 1}, {start_x + 3, initial_pos.y + y_center + 3}, green);
-                }
+                draw_list->AddLine(
+                    {start_x + 3, initial_pos.y + y_center - 1 + 2 * (current_row > 1)}, 
+                    {start_x + 3, initial_pos.y + y_center - 4 + 8 * (current_row > 1)}, green);
+                draw_list->AddLine(
+                    {start_x + 4, initial_pos.y + y_center - 1 + 2 * (current_row > 1)}, 
+                    {start_x + 4, initial_pos.y + y_center - 4 + 8 * (current_row > 1)}, green);
                 ImGui::SetCursorPosX(x);
-                auto const ypos = initial_pos.y + 20 + current_row * ((dial_height-40) / 4.0f);
-                ImGui::SetCursorPosY(initial_pos.y + ypos);
-                if (ImGui::Selectable(presets[ip].c_str(), ip == currently_playing, 0, {3 * dial_width / presets.size(), selectable_height})) {
-                    host_.play(presets[ip]);
-                    currently_playing = ip;
+                ImGui::SetCursorPosY(row_y[current_row]);
+                if (ImGui::Selectable(presets[ip].c_str(), ip == currently_playing, 0, {3 * x_unit, selectable_height})) {
+                    if (currently_playing != ip) {
+                        host_.play(presets[ip]);
+                        currently_playing = ip;
+                    }
+                    else {
+                        host_.stop();
+                        currently_playing = -1;
+                    }
                 }
-                current_row = (3 * current_row + 4) % 5;
+                static constexpr int mapping[] = {3, 2, 0, 1};
+                current_row = mapping[current_row];
             }
-            auto target_x = currently_playing == -1 ? 0 : (currently_playing * dial_width / presets.size() + 3);
+            auto target_x = currently_playing == -1 ? initial_pos.x : (initial_pos.x + currently_playing * x_unit + 2);
             if (dial_x < target_x) {
                 dial_x = std::min(target_x, dial_x + (target_x - dial_x) / 10 + 1);
             }
@@ -63,16 +76,16 @@ namespace radio {
             draw_list->AddLine({0, initial_pos.y + y_center - 1}, {dial_width, initial_pos.y + y_center - 1}, green);
             draw_list->AddLine({0, initial_pos.y + y_center + 1}, {dial_width, initial_pos.y + y_center + 1}, green);
 
+            draw_list->AddTriangleFilled({dial_x + 1, initial_pos.y + 5}, {dial_x - 4, initial_pos.y}, {dial_x + 6, initial_pos.y}, red);
+            draw_list->AddTriangleFilled({dial_x + 1, initial_pos.y + dial_height - 5}, {dial_x - 4, initial_pos.y + dial_height}, {dial_x + 6, initial_pos.y + dial_height}, red);
             draw_list->AddLine({dial_x, initial_pos.y}, {dial_x, initial_pos.y + dial_height}, red);
-            draw_list->AddLine({dial_x + 1, initial_pos.y}, {dial_x + 1, initial_pos.y + dial_height}, red);
+            draw_list->AddLine({dial_x + 2, initial_pos.y}, {dial_x + 2, initial_pos.y + dial_height}, red);
+
+            ImGui::SetCursorPosX(initial_pos.x);
+            ImGui::SetCursorPosY(initial_pos.y + dial_height + 10);
 
             if (host_.has_error()) {
                 ImGui::Text("Error: %s", host_.last_error().c_str());
-            }
-            if (host_.is_playing() && ImGui::Button("Stop"))
-            {
-                host_.stop();
-                currently_playing = -1;
             }
         }
     private:
