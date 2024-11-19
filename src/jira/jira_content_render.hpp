15be3b2852d9695db9460ac8f89f8acf9f80a667
Ignacio Rodríguez <ignacionr@gmail.com>
2024-11-20 01:37:48 +0300

    Refactor Jira content rendering to use std::string instead of std::string_view for better compatibility

--- src/jira/jira_content_render.hpp
+++ src/jira/jira_content_render.hpp
@@ -19,7 +19,7 @@ namespace jira{
                 {"doc", render_subcontent },
                 {"text", [](nlohmann::json::object_t const &el){ 
                     ImGui::SameLine();
-                    ImGui::TextUnformatted(el.at("text").get<std::string_view>().data()); }},
+                    ImGui::TextUnformatted(el.at("text").get<std::string>().data()); }},
                 {"paragraph", [](nlohmann::json::object_t const &el) {
                     ImGui::Text("");
                     render_subcontent(el);
@@ -29,39 +29,31 @@ namespace jira{
                 {"hardBreak", [](nlohmann::json::object_t const &) { ImGui::Text(""); }},
                 {"orderedList", render_subcontent},
                 {"codeBlock", [](nlohmann::json::object_t const &el) {
-                    auto const language = el.at("attrs").at("language").get<std::string_view>();
-                    auto const text = el.at("content").at(0).at("text").get<std::string_view>();
+                    // auto const language = el.at("attrs").at("language").get<std::string>();
+                    auto const text = el.at("content").at(0).at("text").get<std::string>();
                     ImGui::Text("%s", text.data());
                 }},
                 {"panel", render_subcontent},
                 {"table", render_subcontent},
                 {"tableRow", render_subcontent},
                 {"tableCell", render_subcontent},
-                {"inlineExtension", [](nlohmann::json::object_t const &el) {
-                    auto const extension = el.at("attrs").at("extensionType").get<std::string_view>();
-                    auto const text = el.at("content").at(0).at("text").get<std::string_view>();
-                    ImGui::Text("%s", text.data());
-                }},
-                {"inlineCard", [](nlohmann::json::object_t const &el) {
-                    auto const title = el.at("attrs").at("title").get<std::string_view>();
-                    auto const text = el.at("content").at(0).at("text").get<std::string_view>();
-                    ImGui::Text("%s", text.data());
-                }},
                 {"mediaGroup", render_subcontent},
                 {"mediaSingle", render_subcontent},
                 {"media", render_subcontent},
                 {"mediaCard", render_subcontent},
                 {"mediaInline", render_subcontent},
                 {"mention", [](nlohmann::json::object_t const &el) {
-                    auto const text = el.at("attrs").at("text").get<std::string_view>();
-                    ImGui::Text("%s", text.data());
+                    auto const text = el.at("attrs").at("text").get<std::string>();
+                    ImGui::SameLine();
+                    ImGui::TextUnformatted(text.data());
                 }},
                 {"emoji", [](nlohmann::json::object_t const &el) {
-                    auto const text = el.at("attrs").at("text").get<std::string_view>();
-                    ImGui::Text("%s", text.data());
+                    auto const text = el.at("attrs").at("text").get<std::string>();
+                    ImGui::SameLine();
+                    ImGui::TextUnformatted(text.data());
                 }}
             };
-            auto const type = element.at("type").get<std::string_view>();
+            auto const type = element.at("type").get<std::string>();
             if (auto const pos = renderers.find(type); pos != renderers.end()) {
                 pos->second(element);
             }
