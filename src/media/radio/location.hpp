8bc570b80ff706e32f03e945a069fae81fe8c346
Ignacio Rodríguez <ignacionr@gmail.com>
2024-12-30 12:07:06 +0700

    Refactor radio module rendering for improved visual elements and progress display

--- src/media/radio/location.hpp
+++ src/media/radio/location.hpp
@@ -21,20 +21,22 @@ namespace media::radio
                 std::chrono::duration_cast<std::chrono::seconds>(current_run).count() % 60,
                 std::chrono::duration_cast<std::chrono::minutes>(total_run).count(),
                 std::chrono::duration_cast<std::chrono::seconds>(total_run).count() % 60);
-            auto draw_list = ImGui::GetWindowDrawList();
-            float const to_thickness {progress * 20.0f};
-            float const from_thickness {20.0f - to_thickness};
 
+            auto draw_list = ImGui::GetWindowDrawList();
+            static constexpr float max_thickness = 27.0f;
+            static constexpr float reel_radius = 11.0f;
+            static constexpr float M_2PIf = 6.28318530717958647692528676655900576f;
+            float const to_thickness {progress * max_thickness};
+            float const from_thickness {max_thickness - to_thickness};
             ImVec2 const center_from {ImGui::GetCursorPosX() + 50, ImGui::GetCursorPosY() + 68};
             ImVec2 const center_to {ImGui::GetCursorPosX() + 120, ImGui::GetCursorPosY() + 68};
 
             // reel marks
             for (int i = 0; i < 8; i++)
             {
-                static constexpr float M_2PIf = 6.28318530717958647692528676655900576f;
-                auto angle = i * M_2PIf / 8.0f - progress * 18.0f * M_2PIf;
-                float const x1 = 14.0f * cos(angle);
-                float const y1 = 14.0f * sin(angle);
+                auto angle = i * M_2PIf / 8.0f - (M_2PIf * max_thickness) * progress;
+                float const x1 = reel_radius * cos(angle);
+                float const y1 = reel_radius * sin(angle);
                 auto const min = ImVec2{center_from.x + x1 - 1, center_from.y + y1 - 1};
                 auto const max = ImVec2{center_from.x + x1 + 1, center_from.y + y1 + 1};
                 draw_list->AddRectFilled(min, max, IM_COL32(255, 255, 255, 128));
@@ -44,12 +46,26 @@ namespace media::radio
             }
 
             // the reels
-            draw_list->AddCircle(center_from, 15.0f + from_thickness / 2.0f, IM_COL32(255, 255, 0, 255), 24, from_thickness);
-            draw_list->AddCircle(center_to, 15.0f + to_thickness / 2.0f, IM_COL32(255, 255, 0, 255), 24, to_thickness);
+            draw_list->AddCircle(center_from, reel_radius + from_thickness / 2.0f, IM_COL32(255, 255, 0, 255), 36, from_thickness);
+            draw_list->AddCircle(center_to, reel_radius + to_thickness / 2.0f, IM_COL32(255, 255, 0, 255), 36, to_thickness);
+
+            // knobs
+            draw_list->AddCircleFilled({center_from.x - 30.0f, center_from.y + 30.0f}, 4.0f, IM_COL32(255, 255, 0, 255), 6);
+            draw_list->AddCircleFilled({center_to.x + 30.0f, center_to.y + 30.0f}, 4.0f, IM_COL32(255, 255, 0, 255), 6);
+
+            // the tape
+            draw_list->AddLine(
+                {center_from.x - reel_radius - from_thickness, center_from.y}, 
+                {center_from.x - 32.0f, center_from.y + 30.0f}, 
+                IM_COL32(255, 255, 0, 255));
+            draw_list->AddLine(
+                {center_to.x + reel_radius + to_thickness, center_to.y}, 
+                {center_to.x + 32.0f, center_to.y + 30.0f}, 
+                IM_COL32(255, 255, 0, 255));
 
             // cover top and bottom
-            draw_list->AddRectFilled({ImGui::GetCursorPosX(), ImGui::GetCursorPosY()}, {ImGui::GetCursorPosX() + 190, ImGui::GetCursorPosY() + 60}, IM_COL32(0, 0, 0, 170));
-            draw_list->AddRectFilled({ImGui::GetCursorPosX(), ImGui::GetCursorPosY() + 80}, {ImGui::GetCursorPosX() + 190, ImGui::GetCursorPosY() + 140}, IM_COL32(0, 0, 0, 220));
+            draw_list->AddRectFilled({ImGui::GetCursorPosX(), ImGui::GetCursorPosY() + 20}, {ImGui::GetCursorPosX() + 190, ImGui::GetCursorPosY() + 60}, IM_COL32(0, 0, 0, 170));
+            draw_list->AddRectFilled({ImGui::GetCursorPosX(), ImGui::GetCursorPosY() + 85}, {ImGui::GetCursorPosX() + 190, ImGui::GetCursorPosY() + 140}, IM_COL32(0, 0, 0, 170));
             
             // draw progress
             ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 170);
