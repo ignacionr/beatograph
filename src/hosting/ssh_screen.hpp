a4dd42fe89d9a97bfd5e17cb07ce2971387c0a12
Ignacio Rodríguez <ignacionr@gmail.com>
2025-03-13 05:30:23 -0300

    Add process monitoring feature with filtering in SSH screen

--- src/hosting/ssh_screen.hpp
+++ src/hosting/ssh_screen.hpp
@@ -179,6 +179,119 @@ namespace hosting::ssh
                         ImGui::EndTable();
                     }
                 });
+
+                struct process
+                {
+                    process(std::string_view line)
+                    {
+                        auto parts = line | std::views::split(' ') | std::views::filter([](auto const &part) { return !part.empty(); });
+                        auto it = parts.begin();
+                        auto p = *it++;
+                        user = std::string{p.begin(), p.end()};
+                        p = *it++;
+                        pid = std::string{p.begin(), p.end()};
+                        p = *it++;
+                        cpu = std::string{p.begin(), p.end()};
+                        p = *it++;
+                        mem = std::string{p.begin(), p.end()};
+                        p = *it++;
+                        vsz = std::string{p.begin(), p.end()};
+                        p = *it++;
+                        rss = std::string{p.begin(), p.end()};
+                        p = *it++;
+                        tty = std::string{p.begin(), p.end()};
+                        p = *it++;
+                        stat = std::string{p.begin(), p.end()};
+                        p = *it++;
+                        start = std::string{p.begin(), p.end()};
+                        p = *it++;
+                        time = std::string{p.begin(), p.end()};
+                        if (it != parts.end())
+                        {
+                            for (p = *it++; it != parts.end(); p = *it++)
+                            {
+                                command += std::string{p.begin(), p.end()} + " ";
+                            }
+                        }
+                    }
+
+                    std::string user;
+                    std::string pid;
+                    std::string cpu;
+                    std::string mem;
+                    std::string vsz;
+                    std::string rss;
+                    std::string tty;
+                    std::string stat;
+                    std::string start;
+                    std::string time;
+                    std::string command;
+                };
+
+                views::cached_view<std::vector<process>>("Processes", [hostname = host->name(), localhost]() {
+                    auto const result {localhost->ssh("ps aux", hostname)};
+                    std::vector<process> processes;
+                    for (auto const &line : std::string_view{result} | std::views::split('\n') | std::views::drop(1))
+                    {
+                        // break at an empty line
+                        if (line.empty())
+                        {
+                            break;
+                        }
+                        processes.push_back(std::string_view{line.begin(), line.end()});
+                    }
+                    return processes;
+                }, [this](std::vector<process> const &processes) {
+                    if (process_filter_.reserve(200); ImGui::InputText("Filter", process_filter_.data(), process_filter_.capacity()))
+                    {
+                        process_filter_ = process_filter_.data();
+                    }
+
+                    if (ImGui::BeginTable("Processes", 12, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg | ImGuiTableFlags_SizingFixedFit))
+                    {
+                        ImGui::TableSetupColumn("User");
+                        ImGui::TableSetupColumn("PID");
+                        ImGui::TableSetupColumn("CPU");
+                        ImGui::TableSetupColumn("MEM");
+                        ImGui::TableSetupColumn("VSZ");
+                        ImGui::TableSetupColumn("RSS");
+                        ImGui::TableSetupColumn("TTY");
+                        ImGui::TableSetupColumn("STAT");
+                        ImGui::TableSetupColumn("Start");
+                        ImGui::TableSetupColumn("Time");
+                        ImGui::TableSetupColumn("Command");
+                        ImGui::TableHeadersRow();
+                        for (auto const &process : processes | std::views::filter([&](process const &process) { 
+                            return process.command.find(process_filter_) != std::string::npos; }))
+                        {
+                            ImGui::TableNextRow();
+                            ImGui::TableNextColumn();
+                            ImGui::TextUnformatted(process.user.data(), process.user.data() + process.user.size());
+                            ImGui::TableNextColumn();
+                            ImGui::TextUnformatted(process.pid.data(), process.pid.data() + process.pid.size());
+                            ImGui::TableNextColumn();
+                            ImGui::TextUnformatted(process.cpu.data(), process.cpu.data() + process.cpu.size());
+                            ImGui::TableNextColumn();
+                            ImGui::TextUnformatted(process.mem.data(), process.mem.data() + process.mem.size());
+                            ImGui::TableNextColumn();
+                            ImGui::TextUnformatted(process.vsz.data(), process.vsz.data() + process.vsz.size());
+                            ImGui::TableNextColumn();
+                            ImGui::TextUnformatted(process.rss.data(), process.rss.data() + process.rss.size());
+                            ImGui::TableNextColumn();
+                            ImGui::TextUnformatted(process.tty.data(), process.tty.data() + process.tty.size());
+                            ImGui::TableNextColumn();
+                            ImGui::TextUnformatted(process.stat.data(), process.stat.data() + process.stat.size());
+                            ImGui::TableNextColumn();
+                            ImGui::TextUnformatted(process.start.data(), process.start.data() + process.start.size());
+                            ImGui::TableNextColumn();
+                            ImGui::TextUnformatted(process.time.data(), process.time.data() + process.time.size());
+                            ImGui::TableNextColumn();
+                            ImGui::TextUnformatted(process.command.data(), process.command.data() + process.command.size());
+                        }
+                        ImGui::EndTable();
+                    }
+                });
+                ImGui::Unindent();
                 if (ImGui::Button("Connect..."))
                 {
                     // just spawn a new process
@@ -199,5 +312,6 @@ namespace hosting::ssh
         metric_view metric_view_;
         docker_screen docker_screen_;
         std::string systemctl_filter_;
+        std::string process_filter_;
     };
 }
