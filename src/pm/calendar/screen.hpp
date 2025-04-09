5afbb046cf18984a340e3c983689ab710719691b
Ignacio Rodríguez <ignacionr@gmail.com>
2025-04-08 21:57:21 -0300

    Refactor date handling in calendar screen to improve clarity and ensure correct initialization of selected day and month properties

--- src/pm/calendar/screen.hpp
+++ src/pm/calendar/screen.hpp
@@ -17,7 +17,7 @@ namespace calendar
     {
         screen(std::shared_ptr<host> h) : host_(h) {
             today_ = std::chrono::floor<std::chrono::days>(std::chrono::system_clock::now());
-            selected_day_ = today_;
+            set_date(today_);
         }
 
         void render()
@@ -27,34 +27,39 @@ namespace calendar
                 [this](auto const &events) { render_calendar(events); }, true);
         }
 
-        void render_calendar(auto const &events)
-        {
-            static constexpr std::array<std::string_view, 7> days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-            static constexpr std::array<std::string_view, 12> months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        void set_date(std::chrono::system_clock::time_point date) {
+            selected_day_ = date;
             // determine what day of the week the first day of the month is
-            auto const now_time = std::chrono::system_clock::to_time_t(today_);
+            auto const now_time = std::chrono::system_clock::to_time_t(date);
             std::tm tm;
             localtime_s(&tm, &now_time);
-            auto const current_month = tm.tm_mon;
-            auto const current_year = 1900 + tm.tm_year;
+            current_month_ = tm.tm_mon;
+            current_year_ = 1900 + tm.tm_year;
             tm.tm_mday = 1;
             tm.tm_hour = 0;
             tm.tm_min = 0;
             tm.tm_sec = 0;
             std::mktime(&tm);
-            auto const start_of_month{std::chrono::system_clock::from_time_t(std::mktime(&tm))};
-            auto const first_day_of_month = tm.tm_wday;
+            start_of_month_ = std::chrono::system_clock::from_time_t(std::mktime(&tm));
+            first_day_of_month_ = tm.tm_wday;
+            // determine how many days are in the month
+            tm.tm_mday = 32;
+            std::mktime(&tm);
+            days_in_month_ = 32 - tm.tm_mday;
+        }
+
+        void render_calendar(auto const &events)
+        {
+            static constexpr std::array<std::string_view, 7> days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+            static constexpr std::array<std::string_view, 12> months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
 
             ImGui::PushFont(ImGui::GetIO().Fonts->Fonts[2]);
-            auto const month_title{std::format("{} {}", months[current_month], current_year)};
+            auto const month_title{std::format("{} {}", months[current_month_], current_year_)};
             ImGui::TextUnformatted(month_title.c_str());
             ImGui::PopFont();
             ImGui::SameLine();
             ImGui::SetCursorPosX(235.0f);
-            // determine how many days are in the month
-            tm.tm_mday = 32;
-            std::mktime(&tm);
-            auto const days_in_month = 32 - tm.tm_mday;
 
             struct label_and_fn_t {
                 std::string_view label;
@@ -63,8 +68,8 @@ namespace calendar
             };
 
             for (auto const &[text, callback, key] : std::array<label_and_fn_t, 5>{
-                     label_and_fn_t{ICON_MD_SKIP_PREVIOUS, [this, days_in_month] { selected_day_ -= std::chrono::days(week_view_ ? 7 : days_in_month); }, ImGuiKey_LeftArrow},
-                     label_and_fn_t{ICON_MD_SKIP_NEXT, [this, days_in_month] { selected_day_ += std::chrono::days(week_view_ ? 7 : days_in_month); }, ImGuiKey_RightArrow},
+                     label_and_fn_t{ICON_MD_SKIP_PREVIOUS, [this] { selected_day_ -= std::chrono::days(week_view_ ? 7 : days_in_month_); }, ImGuiKey_LeftArrow},
+                     label_and_fn_t{ICON_MD_SKIP_NEXT, [this] { selected_day_ += std::chrono::days(week_view_ ? 7 : days_in_month_); }, ImGuiKey_RightArrow},
                      label_and_fn_t{ICON_MD_TODAY, [this] { selected_day_ = today_; }, ImGuiKey_Home},
                      label_and_fn_t{ICON_MD_CALENDAR_VIEW_MONTH, [this] { week_view_ = false; }, ImGuiKey_M},
                      label_and_fn_t{ICON_MD_CALENDAR_VIEW_WEEK, [this] { week_view_ = true; }, ImGuiKey_W}
@@ -139,13 +144,13 @@ namespace calendar
                     }
                 }
                 else {
-                    for (int i = 0; i < first_day_of_month; ++i)
+                    for (int i = 0; i < first_day_of_month_; ++i)
                     {
                         ImGui::TableNextColumn();
                     }
-                    auto this_day {start_of_month};
+                    auto this_day {start_of_month_};
                     const auto day_height {ImGui::GetWindowHeight() / 7.0f};
-                    for (int i = 1; i <= days_in_month; ++i)
+                    for (int i = 1; i <= days_in_month_; ++i)
                     {
                         ImGui::TableNextColumn();
                         ImGui::PushStyleColor(ImGuiCol_FrameBg, (this_day == today_) ? ImGui::GetStyleColorVec4(ImGuiCol_NavHighlight) : default_bg_color);
@@ -201,5 +206,10 @@ namespace calendar
         std::chrono::system_clock::time_point selected_day_;
         std::chrono::system_clock::time_point today_;
         bool week_view_ {true};
+        int current_month_;
+        int current_year_;
+        int days_in_month_;
+        int first_day_of_month_;
+        std::chrono::system_clock::time_point start_of_month_;
     };
 }
